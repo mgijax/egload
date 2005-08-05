5b300b2e6b62231f5fc3571598137b67a3ea3228
mbw <>
2005-08-05 14:42:30 +0000

    javadocs only

--- java/org/jax/mgi/shr/sva/SVASetConverter.java
+++ java/org/jax/mgi/shr/sva/SVASetConverter.java
@@ -1,14 +1,21 @@
 package org.jax.mgi.shr.sva;
 
 /**
- * <p>@is </p>
- * <p>@has </p>
- * <p>@does </p>
- * <p>@company The Jackson Laboratory</p>
- * @author not attributable
+ * is an object which can convert a given object into SVASet data
+ * and add it to the given SVASet
+ * @has nothing
+ * @does converts data from a given object to SVASet data and adds it to the
+ * given SVASet
+ * @company The Jackson Laboratory
+ * @author M Walker
  *
  */
 
 public interface SVASetConverter {
+    /**
+     * add SVASet data obtained from the given object to the given SVASet
+     * @param obj the given object
+     * @param svaSet the given SVASet
+     */
   public void convert(Object obj, SVASet svaSet);
 }
