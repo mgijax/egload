d2ff67f9bb28ca37a99f1bad80c5e8959ec8c27f
Lori Corbani <lori.corbani@jax.org>
2023-11-08 09:26:09 -0500

    wts2-1324/fl2-625/egload pipeline issue/add sanity check

--- bin/egload.sh
+++ bin/egload.sh
@@ -97,8 +97,9 @@ then
 fi
 
 echo 'Sanity check the radar.dp_entrezgene tables' | tee -a ${LOG}
-${PG_DBUTILS}/bin/testRadarEntrezGene.csh | tee -a ${LOG}
-if [ $status != 0 ] 
+${PG_DBUTILS}/bin/testRadarEntrezGene.csh
+status=$?
+if [ $status -ne 0 ] 
 then
         echo 'radar.dp_entrezgene tables did not pass sanity check; skipping egload' | tee -a ${LOG}
         exit 0
